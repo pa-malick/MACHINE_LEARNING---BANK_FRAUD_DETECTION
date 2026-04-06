75313acfc4590f79e240ef9eaa5df62cd1384f32
Pa-Malick <njaymika@gmail.com>
2026-04-06 21:03:51 +0100

    new commit

--- app/app.py
+++ app/app.py
@@ -105,4 +105,4 @@ def health():
 
 if __name__ == "__main__":
     print("\n🚀  API Flask démarrée sur  http://localhost:5000")
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=5000)
